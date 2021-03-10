70fc70162ec0b4c92a4779967a3597db1407fbd1
rodriss11 <57360904+rodriss11@users.noreply.github.com>
2021-03-10 10:16:31 +0100

    Practica 2

---
+++ Pr2/Enunciado/Secuencia.h
@@ -0,0 +1,54 @@
+﻿/*
+NUMERO DE GRUPO:
+
+NOMBRE Y APELLIDOS DE LOS PARTICIPANTES:
+
+SI ALGUN MIEMBRO DEL GRUPO NO HA PARTICIPADO EN LA PRACTICA,
+EXPLICAR EL MOTIVO:
+
+*/
+
+using namespace std;
+
+#ifndef _SECUENCIA_H_
+#define _SECUENCIA_H_
+
+/// Excepción de elemento inexistente
+class ElementoInvalido {};
+
+
+template<class T>
+class Secuencia {
+public:
+	Secuencia() {
+		// A IMPLEMENTAR
+	}
+
+
+	void pon(const T& e) {
+		// A IMPLEMENTAR
+	}
+
+	const T& elem(int i) {
+		// A IMPLEMENTAR
+	}
+
+	int num_elems() const {
+		// A IMPLEMENTAR
+	}
+
+     // DEBEN INCLUIRSE E IMPLEMENTARSE 
+	 // LAS OPERACIONES ADICIONALES
+	 // NECESARIAS PARA GARANTIZAR EL CORRECTO
+	 // FUNCIONAMIENTO DEL TAD
+
+
+private:
+  // INCLUIR DETALLES DE REPRESENTACION Y OPERACIONES AUXILIARES
+
+};
+
+
+#endif
+
+
---
+++ Pr2/Respuesta/Secuencia.h
@@ -0,0 +1,113 @@
+﻿/*
+NUMERO DE GRUPO:
+
+NOMBRE Y APELLIDOS DE LOS PARTICIPANTES:
+
+
+SI ALGUN MIEMBRO DEL GRUPO NO HA PARTICIPADO EN LA PRACTICA,
+EXPLICAR EL MOTIVO:
+
+*/
+#include <stdlib.h>
+using namespace std;
+
+#ifndef _SECUENCIA_H_
+#define _SECUENCIA_H_
+
+/// Excepción de elemento inexistente
+class ElementoInvalido {};
+
+
+template<class T>
+class Secuencia {
+public:
+	//constructor por defecto
+	Secuencia() {
+		_secuencia = new T[TAM_INICIAL];
+		_tam_actual = 0;
+		_tam_max = TAM_INICIAL;
+	}
+
+	//constructor de copia
+	Secuencia(const Secuencia& s){
+		_secuencia = new T[s._tam_actual];
+		for(int i=0;i<s._tam_actual;i++){
+			_secuencia[i] = s._secuencia[i];
+		}
+
+		_tam_actual = s._tam_actual;
+		_tam_max = s._tam_max;
+	}
+
+	//destructor
+	~Secuencia(){
+		delete[] _secuencia;
+		_secuencia = NULL;
+	}
+
+	//sobrecarga del operador de asignación
+
+	Secuencia& operator =(const Secuencia& s){
+		while(_tam_max<s._tam_actual){
+			redimensionar();
+		}
+		for(int i=0;i<s._tam_actual;i++){
+			_secuencia[i] = s._secuencia[i];
+		}
+		_tam_actual = s._tam_actual;
+		_tam_max = s._tam_max;
+
+		return *this;
+	}
+
+	void pon(const T& e) {
+		if(_tam_actual==_tam_max){
+			redimensionar();
+		}
+		_secuencia[_tam_actual] = e;
+		_tam_actual++;
+	}
+
+	const T& elem(int i) {
+		if(i>=_tam_actual){
+			throw ElementoInvalido();
+		}
+		else{
+			return _secuencia[i];
+		}
+	}
+
+	int num_elems() const {
+		return _tam_actual;
+	}
+
+
+     // DEBEN INCLUIRSE E IMPLEMENTARSE 
+	 // LAS OPERACIONES ADICIONALES
+	 // NECESARIAS PARA GARANTIZAR EL CORRECTO
+	 // FUNCIONAMIENTO DEL TAD
+
+
+private:
+  // INCLUIR DETALLES DE REPRESENTACION Y OPERACIONES AUXILIARES
+
+	static const int TAM_INICIAL = 2;
+	T * _secuencia = NULL;
+	int _tam_actual;
+	int _tam_max;
+
+	void redimensionar(){
+		_tam_max = _tam_max*2;
+		T * aux = new T[_tam_max];
+
+		for(int i=0;i<_tam_actual;i++){
+			aux[i]=_secuencia[i];
+		}
+
+		delete[] _secuencia;
+		_secuencia = aux;
+	}
+};
+
+
+#endif
